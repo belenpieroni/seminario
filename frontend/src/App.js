a1b88535a3e6e5fda741eccc71b1bf8d20acd0f9
belenpieroni <mbpieroni18@gmail.com>
2025-10-09 22:37:25 -0300

    frontend init
    -JS
    -React
    -Tailwind

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Tailwind funciona correctamente 🌀</h1>
+    </div>
+  );
+}
+
+export default App;
